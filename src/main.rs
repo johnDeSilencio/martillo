b2883402702c7ee3fcfe3e1e588994f5ea187da1
Nicholas R. Smith <nicholasrsmith1600@gmail.com>
2023-05-19 22:51:13 -0700

    Define command-line utility interface for martillo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
+static ABOUT_DESCRIPTION: &'static str = "\
+    Rust subcomponent for processing & validating DK-BASIC config files.\n\n\
+    If this utility is being run on on DK-BASIC hardware,\n\
+    it should be run with the \"apply\" parameter; else, it\n\
+    should be run with the \"validate\" parameter.";
+
+#[derive(Parser)]
+#[command(version)]
+#[command(about = ABOUT_DESCRIPTION, long_about=None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    #[command(about = "Apply the settings in mappings.toml (only for use on DK-BASIC hardware)")]
+    Apply {
+        #[arg(help = "The mappings.toml file to apply")]
+        file: PathBuf,
+    }, // the mappings.toml file
+    #[command(about = "Validate the mappings.toml file")]
+    Validate {
+        #[arg(help = "The mappings.toml file to validate")]
+        file: PathBuf,
+    }, // the mappings.toml file
+}
+
 fn main() {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Apply { file } => {
+            println!("I'm applying {:?}", file);
+        }
+        Commands::Validate { file } => {
+            println!("I'm validating {:?}", file);
+        }
+    }
+
     println!("Hello, world!");
 }
